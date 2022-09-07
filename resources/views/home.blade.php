6a3933961f030a85a770fb92e5a6a431a10d3303
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-07 17:03:47 +0200

    Fix: cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.app')
 @section('content')
+    @forelse ($comics as $comic)
+        <div class="col-2 thumb p-4">
+            <div class="img-cutter">
+                <img src="{{$comic->image}}" alt="series"></div>
+            <h5>{{$comic->title}} </h5>
+        </div>
 
-@forelse ($comics as $comic)
-    <div class="col-2 thumb p-4">
-        <div class="img-cutter">
-            <img src="{{$comic->image}}" alt="series"></div>
-        <h5>{{$comic->title}} </h5>
-    </div>
-
-    @empty
-        <h1>Non sono presenti serie</h1>
-@endforelse
-
+        @empty
+            <h1>Non sono presenti serie</h1>
+    @endforelse
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,23 +10,53 @@
 <body>
     @include('includes.header')
     <main>
-              <div class="wrapper">
-                  <div class="jumbotron">
-                    <img src="../assets/img/jumbotron.jpg" alt="">
-                  </div>
-                <div class="container-lg">
-                  <div class="comics row position-relative p-3">
-                    <div class="current-series text-center">Current Series</div>
-                    @yield('content')
-                    {{-- <SingleComic class="col-2" v-for="(item, index) in comicsThumbs" :key="index"
-                    :thumb="item.thumb"
-                    :price="item.price"
-                    :series="item.series"
-                    :type="item.type" /> --}}
-                  </div>
-                </div>
-              </div>
-          </main>
+        <div class="wrapper">
+            <div class="jumbotron">
+            <img src="../assets/img/jumbotron.jpg" alt="">
+            </div>
+        <div class="container-lg">
+            <div class="comics row position-relative p-3">
+            <div class="current-series text-center">Current Series</div>
+            @yield('content')
+            </div>
+        </div>
+        </div>
+        <div class="wrapper2">
+        <div class="p-4 container-lg d-flex justify-content-between">
+            <div class="nav-item d-flex align-items-center">
+                <a href="#">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="buyDigitalComics">
+                <span>DIGITAL COMICS</span>
+                </a>
+            </div>
+            <div class="nav-item d-flex align-items-center">
+                <a href="#">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="buyMerchandise">
+                <span>MERCHANDISE</span>
+                </a>
+            </div>
+            <div class="nav-item d-flex align-items-center">
+                <a href="#">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="buySubscriptions">
+                <span>SUBSCRIPTIONS</span>
+                </a>
+            </div>
+            <div class="nav-item d-flex align-items-center">
+                <a href="#">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="buyDigitalComics">
+                <span>SHOP LOCATOR</span>
+                </a>
+            </div>
+            <div class="nav-item d-flex align-items-center">
+                <a href="#">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="buyVisa">
+                <span>DC POWER VISA</span>
+                </a>
+            </div>
+
+        </div>
+        </div>
+    </main>
 
     @include('includes.footer')
 </body>
