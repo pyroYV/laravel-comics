25870dc60266c007d1ba2ec5e8f0ed75d2b7353e
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-07 17:17:34 +0200

    Feat: Footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,86 @@
+<div id="footer-main">
+    <div class="container-lg">
+    <div class="footer-list">
+        <div id="wrapper3">
+            <h5 class="category">DC Comics</h5>
+                  <ul>
+                    <li class="footer-li"><a href="#">Characters</a></li>
+                    <li class="footer-li"><a href="#">Comics</a></li>
+                    <li class="footer-li"><a href="#">Movies</a></li>
+                    <li class="footer-li"><a href="#">TV</a></li>
+                    <li class="footer-li"><a href="#">Games</a></li>
+                    <li class="footer-li"><a href="#">Videos</a></li>
+                    <li class="footer-li"><a href="#">News</a></li>
+                  </ul>
+                </div>
+                <div class='col-12'>
+                  <h5 class="category">Shop</h5>
+                  <ul>
+                    <li class="footer-li"><a href="#">Shop DC</a></li>
+                    <li class="footer-li"><a href="#">Shop DC Collectibles</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-4 py-5">
+                <h5 class="category">DC</h5>
+                <ul>
+                    <li class="footer-li"><a href="#">Terms Of Use</a></li>
+                    <li class="footer-li"><a href="#">Privacy Policy(New)</a></li>
+                    <li class="footer-li"><a href="#">Ad Choices</a></li>
+                    <li class="footer-li"><a href="#">Advertising</a></li>
+                    <li class="footer-li"><a href="#">Jobs</a></li>
+                    <li class="footer-li"><a href="#">Subscriptions</a></li>
+                    <li class="footer-li"><a href="#">Talent Workshops</a></li>
+                    <li class="footer-li"><a href="#">CPSC Certificates</a></li>
+                    <li class="footer-li"><a href="#">Ratings</a></li>
+                    <li class="footer-li"><a href="#">Shop Help</a></li>
+                    <li class="footer-li"><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+        </div>
+    </div>
+     {{--  <div class="position-absolute" id="logo-bg">
+        <img src="../assets/img/dc-logo-bg.png" alt="dclogo">
+      </div> --}}
+      </div>
+  </div>
+
+<div id="footerS">
+    <div class="container-lg">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-2 ">
+                <a class="sign-up-btn" href="#">SIGN-UP NOW</a>
+            </div>
+            <div id="socials-right" class="col-4 d-flex flex-row align-items-center p-4">
+                <h5>Follow us!</h5>
+                <ul class="d-flex align-items-center">
+                    <li>
+                        <a href="#">
+                            <img src="../assets/img/footer-facebook.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../assets/img/footer-twitter.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../assets/img/footer-youtube.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../assets/img/footer-pinterest.png" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="../assets/img/footer-periscope.png" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
