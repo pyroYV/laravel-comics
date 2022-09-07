af6c828ccc522990e20ae877dd55be50913a4791
pyroYV <simonerizzo.pvt@gmail.com>
2022-09-07 16:12:06 +0200

    Feat: Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,1 +1,42 @@
-<h1>Provasu</h1>
+<header class="container-lg">
+    <div class="row d-flex justfy-content-between">
+        <div class="col-2">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </div>
+        <div class="col-4"></div>
+        <nav class="d-flex col-6">
+            <ul>
+                <li>
+                    characters
+                </li>
+                <li>
+                    comics
+                </li>
+                <li>
+                    movies
+                </li>
+                <li>
+                    tv
+                </li>
+                <li>
+                    games
+                </li>
+                <li>
+                    collectibles
+                </li>
+                <li>
+                    videos
+                </li>
+                <li>
+                    fans
+                </li>
+                <li>
+                    news
+                </li>
+                <li>
+                    shops
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
